d92f57e0bd993a80713fde7b341a6cf7cf6ad895
Jordan <jspeed8th@gmail.com>
2018-10-15 20:38:34 -0400

    created function for wrong guesses

--- index.js
+++ index.js
@@ -1,16 +1,58 @@
-var words = ['Ghoul', 'Gobblin', 'Scare Crow'];
+var wordArray = ['Ghoul', 'Gobblin', 'Scarecrow', 'Ghost', 'Potion'];
+var wordInDashes = '';
+var word;
+var tempWord;
+var wrongGuesses = [];
+
 
 function randomWordChooser() {
-  var index = Math.floor(Math.random() * words.length);
-  return words[index];
+  let index = Math.floor(Math.random() * wordArray.length);
+  return wordArray[index];
 };
 
 function replaceLettersWithDash() {
-  var wordInDashes = '';
-  var word = randomWordChooser()
+  word = randomWordChooser()
+  tempWord = word.split('');
   for(let a = 0; a < word.length; a++) {
-    wordInDashes += ' _ ';
+    wordInDashes += '_';
   }
   console.log(word);
-  return wordInDashes;
+  wordInDashes = wordInDashes.split('');
+}
+
+function guessIt() {
+  wordInDashes = '';
+  replaceLettersWithDash()
+  console.log(wordInDashes);
+  while(word != '') {
+    var letter = prompt('Guess a word!');
+    if(word.includes(letter) == true) {
+      word = word.split(letter).join('');
+      status(letter);
+      console.log(word);
+
+    } else{
+      youLost(letter);
+      alert('Please, try again')
+    }
+  }
+  alert('Congradulations! You guessed ' + tempWord.join(''))
+  console.log(`Incorrect guesses: ${wrongGuesses}`)
+  return 'You Win!'
+}
+
+
+function status(letter) {
+  for(let a = 0; a < tempWord.length; a++) {
+    if(tempWord[a] == letter) {
+      wordInDashes[a] = letter;
+    }
+  }
+  console.log(wordInDashes)
+}
+
+function youLost(letter) {
+  if (!wrongGuesses.includes(letter)) {
+    wrongGuesses.push(letter)
+  }
 }
