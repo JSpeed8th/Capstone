830c1aaf3dc8f8cc245d7d07bf165d69c3e6c2a6
Jordan <jspeed8th@gmail.com>
2018-10-18 09:04:07 -0400

    Added an alert for winning users and retooled if else statements within game

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ var wordContainer = document.querySelector(".word_container");
 var statusOfGame = document.querySelector(".status_of_game");
 var displayWrongGuess = document.querySelector(".wrong_guesses");
 
-var wordArray = ['Ghoul', 'Gobblin', 'Scarecrow', 'Ghost', 'Potion'];
+// var wordArray = ['Ghoul', 'Gobblin', 'Scarecrow', 'Ghost', 'Potion'];
+var wordArray = [
+  {word: 'coffin', hint: 'The person who built it sold it. The person who bought it never used it. The person who used it never saw it. What is it?'},
+  {word: 'blood', hint: 'Dracula loves to draw this, as well as, put it in the bank. What is it?'},
+  {word: 'skeleton', hint: 'I have a body, legs and arms but I have no guts. What am I? '},
+  {word: 'vampire', hint: "No matter what type you are, when I’m thirsty I will come and find you no matter where you are. Who am I?"},
+  {word: 'cemetery', hint: 'You’ll find me in the quietest, creepiest place in town, yet people are dying to get in. What am I?'},
+  {word: 'ghoul', hint: 'A shapeshifting demon that lives in the desert.'}
+]
+
 var wordInDashes = [];
 var word;
 var tempWord;
@@ -13,44 +22,14 @@ var wrongGuesses = [];
 
 function randomWordChooser() {
   let index = Math.floor(Math.random() * wordArray.length);
-  return wordArray[index];
+  return wordArray[index]['word'];
 };
-
-// function replaceLettersWithDash() {
-//   word = randomWordChooser()
-//   tempWord = word.split('');
-//   for(let a = 0; a < word.length; a++) {
-//     wordInDashes += '_';
-//   }
-//   console.log(word);
-//   wordInDashes = wordInDashes.split('');
-// }
 
 function replaceLettersWithDash() {
   word = randomWordChooser()
   tempWord = word.split('');
   console.log(word)
   word.split('').forEach(function(){wordInDashes.push('_')})
-}
-
-function guessIt() {
-  wordInDashes = [];
-  replaceLettersWithDash()
-  statusOfGame.innerText = wordInDashes.join(' ');
-  while(word != '') {
-    var letter = prompt('Guess a word!');
-    if(word.includes(letter) == true) {
-      word = word.split(letter).join('');
-      status(letter);
-      console.log(word);
-
-    } else{
-      youLost(letter);
-      alert('Please, try again')
-    }
-  }
-  console.log(`Incorrect guesses: ${wrongGuesses}`)
-  return 'You Win!'
 }
 
 // ---------------------PRINTS THE STATUS OF THE GAME---------------------------
@@ -68,10 +47,40 @@ function status(letter) {
 
 function youLost(letter) {
   if (!wrongGuesses.includes(letter) && !tempWord.includes(letter)) {
-
     wrongGuesses.push(letter)
     displayWrongGuess.innerText = wrongGuesses;
   }
 }
 
-window.addEventListener('keydown', function(e){console.log(`You just inputted ${e.key}`)});
+
+
+wordInDashes = [];
+replaceLettersWithDash()
+statusOfGame.innerText = wordInDashes.join(' ');
+window.addEventListener('keypress', (letter) => {
+  letter = letter.key;
+  if(word != '') {
+    if(word.length == 1 && word.includes(letter) == true) {
+      word = word.split(letter).join('');
+      status(letter);
+      alert(`YOU WON! Incorrect guesses: ${wrongGuesses}`);
+    }
+    else if(word.includes(letter) == true) {
+      word = word.split(letter).join('');
+      status(letter);
+      console.log(word);
+
+    }else{
+      youLost(letter);
+    }
+  }else {
+    console.log(`Incorrect guesses: ${wrongGuesses}`)
+    console.log('You Win!')
+  }
+})
+
+
+// Functionality Checklist
+// * If player has 6 wrong guesses than add 1 to loss counter
+// * Add hint button.
+// * next each word into an object which includes two hints.
